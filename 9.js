3e6181b07846976b368d97e80f3c44e4e0b5f5b7
ckuthyar <7517545+ckuthyar@users.noreply.github.com>
2025-05-25 19:45:26 +0530

    Two dimensional array - iterate and print the elements

---
+++ 9.js
@@ -0,0 +1,9 @@
+list1 = [[2,4],[3,9],[4,16],[5,25],[6,36]];
+var len1 = list1.length;
+var len2=0;
+for(var j=0;j<len1;j++){
+  len2 = list1[j].length
+  for(var i=0;i<len2;i++){
+    console.log(list1[j][i]);
+  }
+}
