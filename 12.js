068d176ba6acbd3e8ebb8a33f350b7657f3d8103
ckuthyar <7517545+ckuthyar@users.noreply.github.com>
2025-05-25 20:30:32 +0530

    String methods

---
+++ 12.js
@@ -0,0 +1,8 @@
+var s1="FunwithProgramming";
+var s2="SangamOne Connected Ltd";
+console.log(s1.charAt(0));
+console.log(s1.charCodeAt(0));
+console.log(s1.concat(s2));
+console.log(s1.endsWith("Ltd"));
+console.log(s2.endsWith("Ltd"));
+console.log(String.fromCharCode(97));
