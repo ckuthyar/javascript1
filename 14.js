9119574066ab6bac69ae357ea00b30c9e0dfe62e
ckuthyar <7517545+ckuthyar@users.noreply.github.com>
2025-05-25 20:52:51 +0530

    String methods

---
+++ 14.js
@@ -0,0 +1,7 @@
+var s1="FunwithProgramming";
+var s2="SangamOne Connected Ltd";
+console.log(s1.replace(/m/g,"n"));
+console.log(s1.search("mm"));
+console.log(s1.slice(2,4));
+console.log(s1.split("with"));
+console.log(s1.startsWith("Fun"));
