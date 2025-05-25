893feb20da715cb778960d232c843a007570117f
ckuthyar <7517545+ckuthyar@users.noreply.github.com>
2025-05-25 20:48:42 +0530

    String methods

---
+++ 13.js
@@ -0,0 +1,7 @@
+var s1="FunwithProgramming";
+var s2="SangamOne Connected Ltd";
+console.log(s1.includes("Programming"));
+console.log(s1.indexOf("with"))
+console.log(s1.lastIndexOf('m'))
+console.log(s1.match(/m/g));
+console.log(s1.repeat(2));
