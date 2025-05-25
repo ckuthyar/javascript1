e08f5938c4165fd83329f88d8f2ce922aaa84185
ckuthyar <7517545+ckuthyar@users.noreply.github.com>
2025-05-25 20:56:46 +0530

    String methods

---
+++ 15.js
@@ -0,0 +1,6 @@
+var s1 = 'FunwithProgramming';
+var s2 = 'SangamOne Connected Ltd';
+console.log(s1.substring(2, 4));
+console.log(s1.toLowerCase());
+console.log(s1.toUpperCase());
+console.log(s2.trim());
